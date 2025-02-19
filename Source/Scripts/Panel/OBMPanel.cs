253d5c6761fe234c2655c89e1d90aee48621d0ee
bolpen96 <usd_zero@naver.com>
2025-02-19 13:18:03 +0900

    feat : panel
    HandPanel
    LeftPanel
    RightPanel
    OBMPanel

---
+++ Source/Scripts/Panel/OBMPanel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OBMPanel : MonoBehaviour
+{
+    public GameObject Mine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(Mine, 2f);
+    }
+
+}
---
+++ Source/Scripts/Panel/RightPanel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class RightPanel : MonoBehaviour, IDropHandler,IPointerEnterHandler,IPointerExitHandler
+{
+    public Dragable.Slot typeOfItem = Dragable.Slot.Free;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null) return;
+        Dragable d = eventData.pointerDrag.GetComponent<Dragable>();
+
+        if(d != null)
+        {
+            if(typeOfItem == d.typeOfItem || typeOfItem == Dragable.Slot.Free)
+            {
+                d.placeholderParent = this.transform;
+            }
+        }
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null) return;
+        Dragable d = eventData.pointerDrag.GetComponent<Dragable>();
+
+        if(d != null && d.placeholderParent == this.transform)
+        {
+            if(typeOfItem == d.typeOfItem || typeOfItem == Dragable.Slot.Free)
+            {
+                d.placeholderParent = d.parentToReturnTo;
+            }
+        }
+    }
+    public void OnDrop(PointerEventData eventData)
+    {
+        Dragable d = eventData.pointerDrag.GetComponent<Dragable>();
+
+        if(d != null)
+        {
+            if(typeOfItem == d.typeOfItem || typeOfItem== Dragable.Slot.Free)
+            {
+                d.parentToReturnTo = this.transform;
+            }
+        }
+    }
+}
